8c4feec9431df3975f2c08cde83e29f1b0a398d4
Jonathan Ray <jonathanray17@gmail.com>
2019-12-24 02:03:43 -0500

    adds ability to speed up / slow down video playback

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+const speedLookup = {
+	'0.5': 0.75,
+	'0.75': 1,
+	'1': 1.25,
+	'1.25': 1.5,
+	'1.5': 1.75,
+	'1.75': 2,
+	'2': 0.5,
+};
 function playbackTime(e) {
 	if (e.shiftKey) {
-		e.preventDefault();
+		e.stopPropagation();
+		const allVideoElements = [...document.querySelectorAll('video')];
+		allVideoElements.forEach(video => {
+			if (!video.paused) video.playbackRate = speedLookup[video.playbackRate];
+		});
 	}
 	return;
 }
