672a15212e3f5b62cb69e5a94428a92fd9dac870
Jonathan Ray <jonathanray17@gmail.com>
2019-12-24 03:09:43 -0500

    instructions added, ready to be hosted

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-const speedLookup = {
-	'0.5': 0.75,
-	'0.75': 1,
-	'1': 1.25,
-	'1.25': 1.5,
-	'1.5': 1.75,
-	'1.75': 2,
-	'2': 0.5,
-};
-function playbackTime(e) {
-	if (e.shiftKey) {
-		e.stopPropagation();
-		const allVideoElements = [...document.querySelectorAll('video')];
+javascript: (function() {
+	const speedLookup = {
+		'0.5': 0.75,
+		'0.75': 1,
+		'1': 1.25,
+		'1.25': 1.5,
+		'1.5': 1.75,
+		'1.75': 2,
+		'2': 0.5,
+	};
+	const allVideoElements = [...document.querySelectorAll('video')];
+	if (allVideoElements.length === 0) {
+		function playbackTime(e) {
+			if (e.shiftKey) {
+				e.stopPropagation();
+				const allVideoElements = [...document.querySelectorAll('video')];
+				allVideoElements.forEach(video => {
+					if (!video.paused)
+						video.playbackRate = speedLookup[video.playbackRate];
+				});
+			}
+			return;
+		}
+		window.addEventListener('contextmenu', playbackTime);
+	} else {
 		allVideoElements.forEach(video => {
 			if (!video.paused) video.playbackRate = speedLookup[video.playbackRate];
 		});
 	}
-	return;
-}
-window.addEventListener('contextmenu', playbackTime);
+})();
